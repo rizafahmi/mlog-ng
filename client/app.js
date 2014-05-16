900bdd7eae4c5cd8ac88e754f9abb32499731085
Riza Fahmi <rizafahmi@gmail.com>
2014-05-16 22:11:59 +0700

    Add preview to submit post.

--- client/app.js
+++ client/app.js
@@ -10,3 +10,7 @@ App.helpers = {
 _.each(App.helpers, function (helper, key) {
   Handlebars.registerHelper(key, helper);
 });
+
+Meteor.startup(function () {
+  Session.set("MarkdownPreview", "");
+});
