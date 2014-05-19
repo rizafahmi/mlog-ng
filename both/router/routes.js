2aeace6419a58807f30cb5f2e02dca81c1568981
Riza Fahmi <rizafahmi@gmail.com>
2014-05-19 17:34:19 +0700

    Add login system

--- both/router/routes.js
+++ both/router/routes.js
@@ -9,11 +9,18 @@ Router.configure({
   routeControllerNameConverter: 'upperCamelCase'
 });
 
+var loginHook = function (pause) {
+  this.render('Login');
+  pause();
+};
+
+Router.onBeforeAction(loginHook, {only: ['post.submit']});
+
 Router.map(function () {
   this.route('posts.index', {path: '/'});
   this.route('post.submit', {
     path: '/submit',
-    layoutTemplate: 'BlankLayout'
+    layoutTemplate: 'BlankLayout',
   });
   this.route('post.detail', {
     path: '/post/:_id'
---
+++ client/views/shared/login/login.js
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/* Login: Event Handlers and Helpers */
+/*****************************************************************************/
+Template.Login.events({
+  /*
+   * Example: 
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.Login.helpers({
+  /*
+   * Example: 
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* Login: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Login.created = function () {
+};
+
+Template.Login.rendered = function () {
+};
+
+Template.Login.destroyed = function () {
+};
