1c7de13894f8cdf831af45bbf67aef079fd96a2b
Riza Fahmi <rizafahmi@gmail.com>
2014-07-28 11:19:34 +0700

    add RSS feed

---
+++ server/publish/rss/feeds.js
@@ -0,0 +1,20 @@
+RssFeed.publish('feed', function ( query ) {
+  var self = this;
+
+  self.setValue('title', self.cdata('My Article'));
+  self.setValue('description', self.cdata('ElixirDose live feed'));
+  self.setValue('link', 'http://www.elixirdose.com');
+  self.setValue('lastBuildDate', new Date());
+  self.setValue('pubDate', new Date());
+  self.setValue('ttl', 1);
+
+  Posts.find({}, {sort: {datePublish: -1}}).forEach(function(doc) {
+    self.addItem({
+      title: doc.title,
+      description: doc.content,
+      link: 'http://www.elixirdose.com/post/' + doc.slug,
+      pubDate: doc.datePublish
+
+    });
+  });
+});
