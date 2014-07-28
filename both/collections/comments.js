d336c2f915cce0139bac821c02f11a4b4dc040d6
Riza Fahmi <rizafahmi@gmail.com>
2014-07-28 16:45:24 +0700

    Comment finish

---
+++ both/collections/comments.js
@@ -0,0 +1,8 @@
+Comments = new Meteor.Collection('comments');
+
+/*
+ * Add query methods like this:
+ *  Comments.findPublic = function () {
+ *    return Comments.find({is_public: true});
+ *  }
+ */
---
+++ client/views/posts/comment_submit/comment_submit.js
@@ -0,0 +1,54 @@
+/*****************************************************************************/
+/* CommentSubmit: Event Handlers and Helpers */
+/*****************************************************************************/
+Template.CommentSubmit.events({
+  /*
+   * Example: 
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+  'submit form': function (e, tmpl) {
+    e.preventDefault();
+
+    var post = Posts.findOne();
+    var comment = {
+      email: $(e.target).find('[name=email]').val(),
+      comment: $(e.target).find('[name=comment]').val(),
+      dateCreated: new Date(),
+      postid: post._id
+    };
+
+    if(Comments.insert(comment))
+      $(".form")[0].reset();
+
+  }
+});
+
+Template.CommentSubmit.helpers({
+  comments: function () {
+    var post = Posts.findOne({});
+
+    return Comments.find({postid: post._id});
+  }
+});
+
+/*****************************************************************************/
+/* CommentSubmit: Lifecycle Hooks */
+/*****************************************************************************/
+Template.CommentSubmit.created = function () {
+};
+
+Template.CommentSubmit.rendered = function () {
+};
+
+Template.CommentSubmit.destroyed = function () {
+};
+
+UI.registerHelper('split', function (email) {
+  return email.split('@')[0];
+});
+
+UI.registerHelper('formatDate', function (context) {
+  return moment(context).fromNow();
+});
